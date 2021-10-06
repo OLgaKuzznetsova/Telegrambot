771330e5887e3e1edaebc2457ae936aa936c7f74
OLgaKuzznetsova <jk.kuznetsova@gmail.com>
2021-10-06 19:54:04 +0500

    Class Data

--- src/Data.java
+++ src/Data.java
@@ -1,4 +1,17 @@
 public class Data {
     private String term;
     private String definition;
+
+    public void setValues(String iTerm, String iDefinition) {
+        term = iTerm;
+        definition = iDefinition;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
 }
